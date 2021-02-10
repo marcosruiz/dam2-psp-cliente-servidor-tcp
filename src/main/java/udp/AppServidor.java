bf094326b01101c324406dc4ed3cd2bb72e33b59
marcosruiz <marcosruizgarcia@gmail.com>
2021-02-10 18:58:19 +0100

    Update AppServidor.java

--- src/main/java/udp/AppServidor.java
+++ src/main/java/udp/AppServidor.java
@@ -9,8 +9,8 @@ public class AppServidor {
   static final int PUERTO = 4444;
   public static void main(String[] args) throws IOException {
     MulticastSocket multicastSocket = new MulticastSocket(PUERTO);
-    DatagramPacket datagramPacket = new DatagramPacket();
-    multicastSocket.receive(datagramPacket);
-    String mensaje = new String(datagramPacket.getData(), 0, datagramPacket.getLength());
+//    DatagramPacket datagramPacket = new DatagramPacket();
+//    multicastSocket.receive(datagramPacket);
+//    String mensaje = new String(datagramPacket.getData(), 0, datagramPacket.getLength());
   }
 }
